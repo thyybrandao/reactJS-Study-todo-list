4ac0bc6143621768f273ce5a0cc8c9d9bf6d781f
Thyago Brandao de Paula <thyybr@gmail.com>
2023-02-24 16:37:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-console */
+/* eslint-disable react/function-component-definition */
+import React, { useState } from 'react';
+import { MdDelete } from 'react-icons/md';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [value, setValue] = useState('');
+
+  const ENTER_KEY = 13;
+  const ESCAPE_KEY = 27;
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const erase = () => setValue('');
+
+  const submit = () => {
+    setTodos([
+      ...todos,
+
+      {
+        id: new Date().getTime(),
+        title: value,
+        checked: false,
+      },
+    ]);
+    erase();
+  };
+
+  const onKeyDown = (event) => {
+    if (event.which === ENTER_KEY) {
+      submit();
+    } else if (event.which === ESCAPE_KEY) {
+      console.log('Apagar texto!');
+      erase();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section className="container">
+      <header>
+        <h1 className="title">todo</h1>
       </header>
-    </div>
+
+      <section className="main">
+        <input
+          className="new-todo"
+          placeholder="O que precisa ser feito ?"
+          value={value}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          type="text"
+        />
+
+        <ul className="todo-list">
+          {todos.map((todo) => (
+            <li key={todo.id.toString()}>
+              <span className="todo">{todo.title}</span>
+              <button className="remove" type="button">
+                <MdDelete size={28} />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </section>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -11,7 +14,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
